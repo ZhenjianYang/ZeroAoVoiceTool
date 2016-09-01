5bc32d6cf06fb74f6433c85b20a1c403fb234570
Ken Yang <ZJ.Yang@qq.com>
2016-09-01 23:19:24 +0800

    建立基于消息的项目
    使用WTL建立一个窗口程序。预计在这个窗口程序中，用基于消息的方式实现外挂语音

---
+++ src/ZeroAoVoiceToolX/MainDlg.h
@@ -0,0 +1,113 @@
+// MainDlg.h : interface of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CMainDlg : public CDialogImpl<CMainDlg>, public CUpdateUI<CMainDlg>,
+		public CMessageFilter, public CIdleHandler
+{
+public:
+	enum {
+		IDD = IDD_MAINDLG,
+	};
+	enum Status {
+		Idle,
+		Running
+	};
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		return CWindow::IsDialogMessage(pMsg);
+	}
+
+	virtual BOOL OnIdle()
+	{
+		UIUpdateChildWindows();
+		return FALSE;
+	}
+
+	BEGIN_UPDATE_UI_MAP(CMainDlg)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+		MESSAGE_HANDLER(WM_CLOSE, OnClose)
+		COMMAND_HANDLER(IDC_BUTTON_START, BN_CLICKED, OnBnClickedButtonStart)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// center the dialog on the screen
+		CenterWindow();
+
+		// set icons
+		HICON hIcon = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON));
+		SetIcon(hIcon, TRUE);
+		HICON hIconSmall = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON));
+		SetIcon(hIconSmall, FALSE);
+
+		// register object for message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->AddMessageFilter(this);
+		pLoop->AddIdleHandler(this);
+
+		UIAddChildWindowContainer(m_hWnd);
+
+		m_status = CMainDlg::Idle;
+		m_button_start = this->GetDlgItem(IDC_BUTTON_START);
+
+		return TRUE;
+	}
+
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// unregister message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->RemoveMessageFilter(this);
+		pLoop->RemoveIdleHandler(this);
+
+		return 0;
+	}
+
+	LRESULT OnClose(UINT Msg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		this->CloseDialog(0);
+		return 0;
+	}
+
+	void CloseDialog(int nVal)
+	{
+		DestroyWindow();
+		::PostQuitMessage(nVal);
+	}
+	LRESULT OnBnClickedButtonStart(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		// TODO: 在此添加控件通知处理程序代码
+		switch (m_status)
+		{
+		case CMainDlg::Idle:
+			m_button_start.SetWindowTextA("Stop");
+			m_status = CMainDlg::Running;
+			break;
+		case CMainDlg::Running:
+			m_button_start.SetWindowTextA("Start");
+			m_status = CMainDlg::Idle;
+			break;
+		default:
+			break;
+		}
+		return 0;
+	}
+	// 状态
+	Status m_status;
+	CButton m_button_start;
+};
---
+++ src/ZeroAoVoiceToolX/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ZeroAoVoiceToolX.rc 使用
+//
+#define IDR_MAINFRAME                   128
+#define IDD_MAINDLG                     129
+#define IDC_BUTTON_START                1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
